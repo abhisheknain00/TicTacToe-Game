4d5ca845e0318e404262cce5a470eb98c895c762
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-08-02 18:03:51 +0530

    installed .saas and created board and square bare-bones

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const Square = ({ value }) => {
+  return (
+    <button type="buttton" className="square">
+      {value}
+    </button>
+  );
+};
+
+export default Square;
