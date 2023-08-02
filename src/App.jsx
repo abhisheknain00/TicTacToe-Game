e407cc292033097fc0de538c9d40e3a9997ba78c
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-08-02 21:00:56 +0530

    lifted up the state to App component. Show status message, calculate winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import './styles.scss';
 import Board from './components/Board';
+import { useState } from 'react';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+  const nextPlayer = isNext ? 'X' : 'O'; ////DERIVED STATE
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next Player is ${nextPlayer}`;
+
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isNext ? 'X' : 'O';
+        } else {
+          return squareValue;
+        }
+      });
+    });
+
+    setIsNext(currentIsNext => !currentIsNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <h2>{statusMessage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,29 +1,9 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
+
 import Square from './Square';
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isNext, setIsNext] = useState(false);
-
-  const handleSquareClick = clickedPosition => {
-    if (squares[clickedPosition]) {
-      return;
-    }
-
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return isNext ? 'X' : 'O';
-        } else {
-          return squareValue;
-        }
-      });
-    });
-
-    setIsNext(currentIsNext => !currentIsNext);
-  };
-
+const Board = ({ squares, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
