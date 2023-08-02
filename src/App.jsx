a2e130f29f768cf3ef485a1b58e322e47a69a7d0
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-08-03 01:43:55 +0530

    highlighted winning combination

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   const gamingBoard = history[currentMove];
 
-  const winner = calculateWinner(gamingBoard.squares);
+  const { winner, winningSquares } = calculateWinner(gamingBoard.squares);
 
   console.log({ history, currentMove });
 
@@ -66,6 +66,7 @@ function App() {
       <Board
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
       />
 
       <button
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable react/prop-types */
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
+  const colourClassName = value === 'X' ? 'text-green' : 'text-orange';
+  const winningClassName = isWinningSquare ? 'winning' : '';
+
   return (
-    <button type="buttton" className="square" onClick={onClick}>
+    <button
+      type="buttton"
+      className={`square ${colourClassName} ${winningClassName}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
