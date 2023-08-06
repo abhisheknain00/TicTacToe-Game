1ac761f7d73c63bbf1bce3149c7a599892e65998
Pratiksha Kumari <pratikshakumari@Pratikshas-MacBook-Air.local>
2023-08-07 02:49:32 +0530

    removed current typo

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,7 @@ function App() {
           fontWeight: 'normal',
         }}
       >
-        Cuurent Game History
+        Current Game History
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
       <div className="bg-balls" />
